53b1bbaed5ae5385f0e4112d63b15480470f5b01
Alex_Bellamy <alexlbellamy@gmail.com>
2020-06-22 21:00:13 +0200

    adding rspec testing framework, configuring shoulda-matchers, configuring application to avoid generators and amending rspec file

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,14 @@ Bundler.require(*Rails.groups)
 
 module RailsDemo
   class Application < Rails::Application
+    config.generators do |generate|
+      generate.helper false
+      generate.assets false
+      generate.view_specs false
+      generate.helper_specs false
+      generate.routing_specs false
+      generate.controller_specs false
+    end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
