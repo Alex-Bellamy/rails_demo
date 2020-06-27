f2d61445745937fe77b94530501d6e84172d651a
Alex_Bellamy <alexlbellamy@gmail.com>
2020-06-27 15:31:12 +0200

    updating articles controller, application view and routes to enable user to create articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,5 +6,16 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
-  
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.create(params.require(:article).permit(:title, :content))
+    if @article.persisted?
+      redirect_to root_path, notice: 'Article created!'
+    else redirect_to new_article_path, notice: 'Error try again'
+    end
+end
 end
--- spec/features/user_can_write_new_article_spec.rb
+++ spec/features/user_can_write_new_article_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
-feature 'User can write new article'
+feature 'User can write new article' do
 before do
     visit root_path
     click_on 'Write Article'
-    fill_in 'Title', with: 'You sit at tables'
-    fill_in 'Content', with: 'and sit on chairs, yes chairs'
+    fill_in 'Title', :with => 'Tables'
+    fill_in 'Content', :with => 'and sit on chairs, yes chairs'
     click_on 'Create Article'
 end
 
@@ -13,4 +13,5 @@ describe 'User can write an article' do
     it 'User should see success message' do
         expect(page).to have_content 'Article created!'
     end
+end
 end
