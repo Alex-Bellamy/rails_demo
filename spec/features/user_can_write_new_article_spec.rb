dcaca076c65814f747f3932665e487ca764b9923
Alex_Bellamy <alexlbellamy@gmail.com>
2020-06-28 21:54:15 +0200

    amending spec so that only a logged in user can create an article and amending view so that the flash only appears once

--- spec/features/user_can_write_new_article_spec.rb
+++ spec/features/user_can_write_new_article_spec.rb
@@ -1,15 +1,20 @@
 require 'rails_helper'
 
-feature 'User can write new article' do
+feature 'Logged in user can write new article' do
 before do
+    create(:user, email: 'andre@mail.com', password: '123456') 
     visit root_path
+    click_on 'Sign in/Sign Up'
+    fill_in 'Email', :with => 'andre@mail.com'
+    fill_in 'Password', :with => '123456'
+    click_on 'Log in'
     click_on 'Write Article'
-    fill_in 'Title', :with => 'Tables'
-    fill_in 'Content', :with => 'and sit on chairs, yes chairs'
+    fill_in 'Title', :with => 'Kitchens'
+    fill_in 'Content', :with => 'Dine in them'
     click_on 'Create Article'
 end
 
-describe 'User can write an article' do
+describe 'Signed in user can create an article' do
     it 'User should see success message' do
         expect(page).to have_content 'Article created!'
     end
